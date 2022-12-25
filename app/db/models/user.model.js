3d533a58aed9e06a4668504d1b33114fd14b70ae
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2022-12-25 13:47:41 +0200

    User schema updated

--- app/db/models/user.model.js
+++ app/db/models/user.model.js
@@ -1,82 +1,84 @@
 const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
-    fName:{
-        type:String, 
-        trim:true,
-        lowercase:true,
+    fName: {
+        type: String,
+        trim: true,
+        lowercase: true,
         minLength: 5,
-        maxLength:20,
-        required:true
-    }, 
-    lName:{
-        type:String, 
-        trim:true,
-        lowercase:true,
+        maxLength: 20,
+        required: true
+    },
+    lName: {
+        type: String,
+        trim: true,
+        lowercase: true,
         minLength: 5,
-        maxLength:20,
-        required:true
-    }, 
-    age:{
-        type:Number,
-        min:21,
-        max:60,
-        default:21
-    }, 
-    email:{
-        type:String, 
-        trim:true,
-        lowercase:true,
-        required:true,
+        maxLength: 20,
+        required: true
+    },
+    age: {
+        type: Number,
+        min: 21,
+        max: 60,
+        default: 21
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        required: true,
         unique: true,
-        validate(value){
-            if(!validator.isEmail(value)){
+        validate(value) {
+            if (!validator.isEmail(value)) {
                 throw new Error("invalid email format")
             }
         }
-    }, 
-    status:{
-        type:Boolean,
+    },
+    status: {
+        type: Boolean,
         default: false
-    }, 
-    image:{
-        type:String, 
-        trim:true
-    }, 
-    password:{
-        type:String, 
-        trim:true,
-        minLength: 5,
-        required:true,
-        // match: ''
-    }, 
-    gender:{
-        type:String, 
-        trim:true,
-        lowercase:true,
-        enum: ["male", "female"]
-    }, 
-    dOfBirth:{
-        type: Date
-    }, 
-    phoneNum:{
+    },
+    image: {
         type: String,
-        validate(value){
-            if(!validator.isMobilePhone(value, "ar-EG"))
-                throw new Error ("invalid number")
+        trim: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        minLength: 5,
+        required: true,
+        // match: ''
+    },
+    gender: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        enum: ["male", "female"]
+    },
+    dOfBirth: {
+        type: Date
+    },
+    phoneNum: {
+        type: String,
+        validate(value) {
+            if (!validator.isMobilePhone(value, "ar-EG"))
+                throw new Error("invalid number")
         }
     },
     addresses: [
         {
-            addressType:{
-                type:String, 
-                trim:true,
-                required:true
+            addressType: {
+                type: String,
+                trim: true,
+                required: true
             },
-            details:{}
+            details: {}
         }
-    ], 
-
+    ],
+    ruleId: {
+        String
+    }
 
 })
 
@@ -88,4 +90,4 @@ const userSchema = mongoose.Schema({
 
 
 const User = mongoose.model("User", userSchema)
-module.exports=User
+module.exports = User
